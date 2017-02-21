bb8a4a3644128feeeff6b47eda91d879f552dd48
Thomas Sauer <Thomas Sauer>
2017-02-21 14:24:22 -0500

    initialize git repo, player movements

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    public float jumpHeight;
+    public Transform GroundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatisground;
+    private bool grounded;
+
+    [SerializeField]
+    private float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+    void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(GroundCheck.position, groundCheckRadius, whatisground);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown (KeyCode.Space) && grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+
+	}
+}
