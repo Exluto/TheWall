4a07ffdadf0ef7ee487e1ed1fadb8830f727daf3
Jaype27 <jaypee.0927@gmail.com>
2019-09-28 13:52:11 -0400

    wall tileset

--- Assets/Scripts/NewScripts/LaserBeam.cs
+++ Assets/Scripts/NewScripts/LaserBeam.cs
@@ -6,9 +6,22 @@ public class LaserBeam : MonoBehaviour, IPooledObject {
 
 	public float _speed = 20f;
 	public Rigidbody2D _rb;
+	public float _activeDuration = 2.0f;
+	private float _activeTimer;
+
+	void OnEnable() {
+		_activeTimer = _activeDuration;
+	}
 
 	// Use this for initialization
 	public void OnObjectSpawn () {
-		_rb.velocity = transform.right * _speed;
+		_rb.velocity = transform.right * _speed;	
+	}
+
+	void Update() {
+		_activeTimer -= Time.deltaTime;
+		if(_activeTimer <= 0f) {
+			this.gameObject.SetActive(false);
+		}
 	}
 }
